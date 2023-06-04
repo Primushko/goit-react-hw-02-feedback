8dfdadd04e32566e468c4ad16c779a575573ef03
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-06-04 19:03:02 +0300

    onovlenny 2

--- src/components/FreedbackOptions/FeedbackOptions.jsx
+++ src/components/FreedbackOptions/FeedbackOptions.jsx
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
 import s from '../FreedbackOptions/FeedbackOptions.module.css';
-
+// компонент "FeedbackOptions" приймає два пропи: "options" і "onLeaveFeedback".
+// призначений для відображення набору кнопок зворотного зв'язку і передачі вибраного
+// варіанту зворотного зв'язку батьківському компоненту для подальшої обробки.
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  //- "options" є об'єктом, який містить різні варіанти зворотного зв'язку ("Good", "Bad", "Neutral").
+  //- "onLeaveFeedback" - функцією зворотнього виклику, яка викликається при натисканні кнопки зворотного зв'язку.
   <div className={s.feedback__container}>
     {Object.keys(options).map(option => (
       <button
@@ -16,7 +20,9 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
     ))}
   </div>
 );
-
+//PropTypes для валідації пропів.
+//PropTypes.object.isRequired вказує, що "options" повинен бути об'єктом,
+//PropTypes.func.isRequired вказує, що "onLeaveFeedback" повинен бути функцією і є обов'язковим пропом.
 FeedbackOptions.propTypes = {
   options: PropTypes.object.isRequired,
   onLeaveFeedback: PropTypes.func.isRequired,
